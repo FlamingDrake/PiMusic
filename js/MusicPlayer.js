a4194b2f7858b9dccdd90504269bf8f62635afbb
FlamingDrake <robin.ljungstrom@gmail.com>
2018-09-08 19:16:18 +0200

    Channel Chooser

--- js/MusicPlayer.js
+++ js/MusicPlayer.js
@@ -1,10 +1,29 @@
 let audio = new Audio('http://sverigesradio.se/topsy/direkt/207-hi-mp3');
 
+
 function playMusic() {
-    audio.play().then();
+    audio.play();
 }
 
 function pauseMusic() {
     audio.pause();
 }
+
+function p4() {
+    audio.src = 'http://sverigesradio.se/topsy/direkt/207-hi-mp3';
+    audio.play();
+}
+
+function guld() {
+    audio.src = "http://stream.dbmedia.se/gkMP3";
+    audio.play();
+}
+
+function nrj() {
+    audio.src = "http://tx-bauerse.sharp-stream.com/http_live.php?i=nrj_instreamtest_se_mp3";
+    audio.play();
+
+}
+
+
 
