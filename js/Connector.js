ebc7e3b7b5b8dea674e50ac568fa227f39e78cc7
FlamingDrake <robin.ljungstrom@gmail.com>
2018-09-12 18:02:16 +0200

    Backend Done

---
+++ js/Connector.js
@@ -0,0 +1,58 @@
+const port = 8000;
+
+let currChannel = document.getElementById('currChannel');
+
+const url = 'http://localhost:8000';
+
+const http = new XMLHttpRequest();
+
+/*
+STREAM-ENDPOINTS
+
+P4
+http://sverigesradio.se/topsy/direkt/207-hi-mp3';
+
+Guld
+http://stream.dbmedia.se/gkMP3
+
+NRJ
+http://tx-bauerse.sharp-stream.com/http_live.php?i=nrj_instreamtest_se_mp3
+
+ */
+
+let p4Data = {station: 'http://sverigesradio.se/topsy/direkt/207-hi-mp3'};
+
+let guldData = {station: 'http://stream.dbmedia.se/gkMP3'};
+
+let nrjData = {station: 'http://tx-bauerse.sharp-stream.com/http_live.php?i=nrj_instreamtest_se_mp3'};
+
+function playMusic() {
+    http.open('POST', url + '/play', true);
+    http.send();
+}
+
+function pauseMusic() {
+    http.open('POST', url + '/pause');
+    http.send();
+}
+
+function guld() {
+    currChannel.innerText = 'Guld Kanalen';
+    http.open('POST', url + '/change', true);
+    http.setRequestHeader("Content-type", "application/json");
+    http.send(JSON.stringify(guldData));
+}
+
+function p4() {
+    currChannel.innerText = 'P4';
+    http.open('POST', url + '/change', true);
+    http.setRequestHeader("Content-type", "application/json");
+    http.send(JSON.stringify(p4Data));
+}
+
+function nrj() {
+    currChannel.innerText = 'NRJ';
+    http.open('POST', url + '/change', true);
+    http.setRequestHeader("Content-type", "application/json");
+    http.send(JSON.stringify(nrjData));
+}
--- js/MusicPlayer.js
+++ js/MusicPlayer.js
@@ -1,29 +1,63 @@
-let audio = new Audio('http://sverigesradio.se/topsy/direkt/207-hi-mp3');
+const {spawn} = require('child_process');
+const express = require('express');
+
+const port = 8000;
+let radio;
+
+let currentStation = 'http://sverigesradio.se/topsy/direkt/207-hi-mp3';
+
+let app = express();
+
+let alreadyPlaying = false;
 
 
-function playMusic() {
-    audio.play();
-}
+app.listen(port, () => {
+    console.log("Server running on port " + port);
+});
 
-function pauseMusic() {
-    audio.pause();
-}
+app.use(express.json());
 
-function p4() {
-    audio.src = 'http://sverigesradio.se/topsy/direkt/207-hi-mp3';
-    audio.play();
-}
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    next();
+});
 
-function guld() {
-    audio.src = "http://stream.dbmedia.se/gkMP3";
-    audio.play();
-}
+let play = express.Router();
 
-function nrj() {
-    audio.src = "http://tx-bauerse.sharp-stream.com/http_live.php?i=nrj_instreamtest_se_mp3";
-    audio.play();
+play.post('/play', function (req, res, next) {
+    req.accepts('application/json');
+    if (!alreadyPlaying) {
+        alreadyPlaying = true;
+        radio = spawn('C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe', [currentStation]);
+    }
+    res.end();
+});
 
-}
 
+let pause = express.Router();
+pause.post('/pause', function (req, res, next) {
+    if (alreadyPlaying) {
+        alreadyPlaying = false;
+        radio.kill(9);
+    }
+    res.end();
+});
 
+let changeStation = express.Router();
+changeStation.post('/change', function (req, res) {
+    currentStation = req.body.station;
+    if(alreadyPlaying){
+       radio.kill(9);
+       radio = spawn('C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe', [currentStation]);
+    } else {
+        alreadyPlaying = true;
+        radio = spawn('C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe', [currentStation]);
+    }
+    res.end();
+});
 
+app.use(play);
+app.use(pause);
+app.use(changeStation);
